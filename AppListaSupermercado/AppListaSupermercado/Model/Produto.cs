995aa463eb8aaec481807e0fb21512c25352b8f7
PabloaLenda <pablogomesjau@gmail.com>
2022-06-13 08:14:34 -0300

    Arquivos Enviados

---
+++ AppListaSupermercado/AppListaSupermercado/Model/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+         namespace AppListaSupermercado.Model
+{
+      public class Produto
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public double Quantidade { get; set; }
+        public double ValorEstimado { get; set; }
+        public double TotalEstimado { get; set; }
+        public double? ValorReal { get; set; }
+        public double? TotalReal { get; set; }
+       
+    }
+}
